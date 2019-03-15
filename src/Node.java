823b32d370a583037ee647b4dd9aa0e10d31399c
Alexandre <alexandre707@live.com>
2019-03-15 01:12:21 -0400

    Added user interface class and moved files around

---
+++ src/Node.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 100657250
+ */
+public class Node {
+    private int x;
+    private int y;
+    
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+        
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+}
---
+++ src/PrioritySearchTree.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 100657250
+ */
+public class PrioritySearchTree {
+    private int size = 0;
+    private Node[] H = new Node[10];
+    
+    
+    private int Parent(int i) {
+        return (int) Math.floor(i / 2);
+    }
+
+    private int LeftChild(int i) {
+        return 2 * i;
+    }
+
+    private int RightChild(int i) {
+        return 2 * i + 1;
+    }
+    
+    public void SiftUp(Node i){ //Questionable
+       int y = i.getY();
+        while(y > 1 && H[Parent(y)].getY() < H[y].getY()){
+            Node temp = H[y];
+            H[y] = H[Parent(y)];
+            H[Parent(y)] = temp;
+            i = H[Parent(y)];
+       }
+    } 
+        
+    public void Insert(Node t) {
+         size = size + 1;
+            H[size] = t;
+            SiftUp(H[size]);
+        
+    }
+
+}
