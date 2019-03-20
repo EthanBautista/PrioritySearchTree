f94a2ce8f200fdecde58a950f8e2e1ac57c05e10
EthanBautista <43651657+EthanBautista@users.noreply.github.com>
2019-03-20 01:17:59 -0400

    Split function needs to be fixed

---
+++ UserInterface.java
@@ -0,0 +1,46 @@
+public class UserInterface {
+    public static void main(String[] args) {
+
+        //Trees holding nodes
+        PrioritySearchTree pst = new PrioritySearchTree();
+        BinaryTree bt = new BinaryTree();
+
+        //Define nodes
+        Node n0 = new Node(1, 100);
+        Node n1 = new Node(2, 50);
+        Node n2 = new Node(4, 3);
+        Node n3 = new Node(3, 5);
+        Node n4 = new Node(7, 2);
+        Node n5 = new Node(5, 8);
+        Node n6 = new Node(10, 2);
+        Node n7 = new Node(20, 51);
+
+        //Add nodes to bt
+        bt.Insert(n0);
+        bt.Insert(n1);
+        bt.Insert(n2);
+        bt.Insert(n3);
+        bt.Insert(n4);
+        bt.Insert(n5);
+        bt.Insert(n6);
+        bt.Insert(n7);
+        
+        bt.SiftLeft();
+        //print bt
+        bt.Print();
+
+        //remove nodes from bt & add to pst
+        transfer_bt_bst(bt, pst);
+
+        //print pst
+        pst.Print();
+
+    }
+
+    private static void transfer_bt_bst(BinaryTree ibt, PrioritySearchTree opst) {
+        for (int i = 0; i != ibt.size(); i = i) {
+            opst.Insert(ibt.Remove());
+        }
+    }
+    
+}
